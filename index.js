06d0860a16de7a472708fa49d722d2e30e08f811
ellavrubel <webella@protonmail.com>
2020-10-23 17:24:05 +0300

    lesson 3 -  arrays

--- index.js
+++ index.js
@@ -1,166 +1,37 @@
-    // 'use strict';
+     //
+     //
+     // const month = ['April', 'June', 'March']; // литерал массива
+     // const monthLength = month.length;
+     //
+     // console.log(month);
+     // console.log(monthLength);
+     //
+     // //
+     //
+     // const neo = new Array(5); // конструктор массива
+     //
+     // const neoLength = neo.length;
+     //
+     // console.log(neo);
+     // console.log(neoLength);
+     //
+     // //
+     // const a = new Array(5.5);
+     //
+     // const aLength = a.length;
+     //
+     // console.log(a); // RangeError: Invalid array length
+     // console.log(aLength);
+     //
+     // //
 
+//     ES5 Duck typing проверка массива
 
-        const greetings = 'Hello, it is me!';
+     const m = ['June', 'July', 'March'];
 
-        console.log(greetings);
+     if (typeof m === 'object' && m.splice){
 
+         console.log('-> This is an array!');
 
-
-    //    use strict in function
-
-        function sum(a, b) {
-            'use strict';
-
-            return (a + b);
-
-        }
-    //
-
-        console.log(sum (4,5));
-
-        d = 3 + 4;
-        console.log(d);
-
-        // hosting
-
-        console.log(age); // undefind
-        var age = 10;
-
-        console.log(age2); // ReferenceError
-        const age2 = 10; // let works the same
-
-    // TDZ -  temporal dead zone
-
-    if (true){
-        console.log(typeof value);
-
-        let value = 1;
-    }
-
-    //
-
-
-    const firstName = 'Ella'; // Camel Case
-    const last_name = 'Vrubel'; // snake_case
-
-
-//    при сложении дробных чисел
-
-    const s = 0.1;
-    const y = 0.2;
-
-    const summer = s + y;
-    console.log(summer); // 0.30000000000000004
-
-//    чтобы этого избежать
-
-    const num1 = 0.1;
-    const num2 = 0.2;
-
-    const result = (num1 * 10 + num2 * 10) / 10;
-    console.log(result);
-
-
-
-    // boolean
-
-    const test = Boolean(false); // false
-//
-
-//    склеивание строк в ES6
-
-    const fN = 'Olga';
-    const lN = 'Fedorova';
-
-    const fullN = `Ms. ${fN} ${lN}`;
-    console.log(fullN);
-
-//
-
-
-    let G = 1;
-
-    console.log(++G); // 2
-    console.log(G++); // 2
-    console.log(G);   // 3
-
-
-//    if - else if -else if
-
-    if (3 > 8){
-        console.log('Horray!');
-    } else {
-        console.log('False');
-    }
-
-
-
-    if ( 5 > 9){
-        console.log('case 1');
-    } else if (3 < 10) {  // возможность указать еще одно условие
-        console.log('case 2');
-    } else {
-        console.log('Nothing is working');
-    }
-
-//
-
-//    тернарный оператор
-
-    const year = 16;
-    const store = year >= 18 ? true : false;
-
-    if (store) {
-        console.log('open');
-    } else {
-        console.log('closed');
-    }
-
-//    альтернативный вариант записи
-
-//     if (store) {
-//         return console.log('open'); // return  прерывает выполнение кода
-//     }
-    // console.log('close');
-
-
-//
-
-//    Switch - case
-
-    const dayNumber = 1;
-    let day;
-
-    switch (dayNumber) {
-        case 1:
-            day = 'Sunday';
-            break;
-        case 2:
-            day = 'Monday';
-            break;
-        default:
-            day = null;
-    }
-
-    console.log(day); // Sunday
-
-//
-
-//    вложенность
-
-    const sirName = 'Vrubel';
-
-    if (sirName){
-        if (sirName === 'Fedorova'){
-            console.log(sirName);
-        } else {
-            console.log('I do not know your sirName');
-        }
-    } else {
-        console.log('Type error');
-    }
-
-//
-
+     }
 
