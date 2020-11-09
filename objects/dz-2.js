f2d12cb041361767ce876f57c0a78c9c31069d9b
ellavrubel <webella@protonmail.com>
2020-11-09 18:04:52 +0300

    Objects - dz2 in prog

---
+++ objects/dz-2.js
@@ -0,0 +1,53 @@
+'use strict';
+
+                  // 1. Cоздать объект person.
+
+                  //   Обратите внимание!
+
+                  //   1. Объект person должен иметь свойство name.
+                  // 2. Cвоство name должно быть не меньше 3 символов.
+
+                  // 3. Свойство roles которое является массивом в котором перечислены роли сотрудника.
+                  //   Например — Developer, Project Manager.
+                  // 6. Если массив roles пустой, должна вернуться строка — Сотруднику John Doe не
+                  // // назначены роли.
+
+                  // 4. Объект person должен иметь метод toString кторый будет возвращать строку —
+                  // Сотрудник John Doe занимает следующие позиции в компании: Developer, Project
+                  // Manager.
+                  // 5. Строка которая возвращается при вызове методы toString должна брать данные по
+                  // имени и занимаемым позициям из полей person и roles.
+                  //
+
+                  // 7. Объект person должен быть родителем для объекта staff.
+
+
+  const person = {
+
+
+    set name (value){
+      if(typeof value !== 'string' || value < 3) throw new Error(`Only strings >= 3 letters allowed!`);
+      this._name = value;
+    },
+    get name(){
+      return this._name;
+    },
+
+    roles:[],
+
+    toString (){
+      return `Сотрудник ${this.name} занимает следующую позицию в компании: ${this.roles}`;
+    }
+  };
+
+
+  person.name = 'Ella Vrubel';
+  person.roles = ['Web- Developer'];
+
+
+  console.log(person.toString()); // Сотрудник Ella Vrubel занимает следующую позицию в компании: Web- Developer
+
+
+  const staff = Object.create(person);
+  console.log(staff.name);  // Ella Vrubel
+
